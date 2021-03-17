913f67254bf42deb51f4be5a83392dafb58d41e1
Aleksandra Azarova <alekssaffl@yandex.ru>
2021-03-17 22:26:21 +0300

    add: recursive depth, extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, {repeatTimes = 1, separator = '+', addition = '', additionRepeatTimes = 1, additionSeparator = '|'}) {
+  const res = [];
+  const add = [];
+
+  if (addition !== '') {
+    for (let i = 0; i < additionRepeatTimes; i += 1) {
+      add.push(`${addition}`);
+    }
+  }
+
+  for (let i = 0; i < repeatTimes; i += 1) {
+    res.push(`${add.length !== 0 ? str + add.join(additionSeparator) : str}`);
+  }
+
+  return res.join(separator);
 };
   
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    if (arr.length === 0) {
+      return 1;
+    }
+    const nests = [];
+    for (let item of arr) {
+      let res = 1;
+      if (Array.isArray(item)) {
+        res += this.calculateDepth(item);
+      }
+      nests.push(res);
+    }
+    return nests.sort((a, b) => b - a)[0];
   }
 };

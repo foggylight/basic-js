f65759b5d5c4b77ad73df87317ce4fc1a90e9636
Aleksandra Azarova <alekssaffl@yandex.ru>
2021-03-18 03:29:42 +0300

    add: vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,56 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(mode = true) {
+    this.mode = mode === true ? 'direct' : 'reverse';
+    this.chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+
+  getStr(str) {
+    return str.toUpperCase().split('');
+  }
+
+  getKey(str, key) {
+    let formatedKey = '';
+    for (let i = 0, k = 0; i < str.length; i += 1, k += 1) {
+      if (this.chars.indexOf(this.getStr(str)[i]) === -1) {
+        formatedKey += ' ';
+        k -= 1;
+      } else {
+        const keyIndex = k < key.length ? k : k % key.length;
+        formatedKey += key[keyIndex];
+      }
+    };
+    return formatedKey.toUpperCase().split('').map((char) => this.chars.indexOf(char));
+  }
+
+  engine(str = null, key = null, type) {
+    if (str === null || key === null) {
+      throw new Error('no arguments');
+    }
+    const formatedKey = this.getKey(str, key);
+    const res = this.getStr(str).map((char, i) => {
+      const charIndex = this.chars.indexOf(char);
+      if (charIndex === -1) {
+        return char;
+      };
+      const shift = formatedKey[i];
+      switch(type) {
+        case 'encrypt':
+          return this.chars[charIndex + shift >= 26 ? (charIndex + shift) - 26 : charIndex + shift];
+        case 'decrypt':
+          return this.chars[charIndex + 26 - shift >= 26 ? charIndex - shift : charIndex + 26 - shift];
+      };
+    });
+    return this.mode === 'direct' ? res.join('') : res.reverse().join('');
+  }
+
+  encrypt(str, key) {
+    return this.engine(str, key, 'encrypt');
+  }
+
+  decrypt(str, key) {
+    return this.engine(str, key, 'decrypt');
   }
 }
 
